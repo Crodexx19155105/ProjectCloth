642a88ead6f97bcba3795305d4c500ce8737513c
Utkarsh-Agarwal-IITBHU <utkarshagarwal2424@gmail.com>
2021-07-21 10:49:04 +0530

    randomized shop page

--- src/Component/ShopeComponent/ShopLists.js
+++ src/Component/ShopeComponent/ShopLists.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ShopItem from './ShopItem'
 
 function ShopLists({title,items}) {
+    const [randArr, setrandArr] = useState([]);
+    const myset=new Set();
+    useEffect(() => {
+        while(myset.size!=4) {
+            var num=Math.floor(Math.random()*8) + 1;
+            myset.add(num);
+            
+        }
+        const myArr = Array.from(myset);
+        setrandArr(myArr);
+    }, [])
     return (
         <div>
             <div style={{fontSize:"30px",fontWeight:"bold"}}>{title}</div>
@@ -9,7 +20,7 @@ function ShopLists({title,items}) {
             <div className="Item_GridContainer">
                 
             {items.map((prod)=>
-            (prod.id<=4) &&<ShopItem key={prod.id} {...prod}/>)}
+            (randArr.some((el)=>el==prod.id)) &&<ShopItem key={prod.id} {...prod}/>)}
             
 
 
