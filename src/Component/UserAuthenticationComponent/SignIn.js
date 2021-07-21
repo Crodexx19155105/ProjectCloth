61a059c25bf5dd90199d1935273c8c6ea2e818cc
Utkarsh-Agarwal-IITBHU <utkarshagarwal2424@gmail.com>
2021-07-21 20:52:42 +0530

    Finally Completing the Sign-Up and Sign-In Component

--- src/Component/UserAuthenticationComponent/SignIn.js
+++ src/Component/UserAuthenticationComponent/SignIn.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 import Button from '../buttonComponenet/button';
 import FormComp from '../Form/FormComp';
-import { SignUpWithGoogle} from '../firebase/firebase.utils.js'
+import { auth, SignUpWithGoogle} from '../firebase/firebase.utils.js'
 
 
 function SignIn() {
+    const [UserDetails, setUserDetails] = useState({
+        Email:'',
+        Password:'',
+    })
     const handlesign=()=>{
         try {
             
@@ -13,18 +17,19 @@ function SignIn() {
             console.error();
         }
     }
-    const [UserDetails, setUserDetails] = useState({
-        Email:'',
-        Password:'',
-    })
-
-
-    const handleSubmit=(e)=>{
+    const handleSignINEmailandPass=(e)=>{
         e.preventDefault();
-        setUserDetails({
-            Email:'',
-        Password:'',
-        })
+        try {
+            const {Email,Password}=UserDetails;
+            
+            auth.signInWithEmailAndPassword(Email,Password);
+            setUserDetails({
+                Email:'',
+            Password:'',
+            })
+        } catch (error) {
+            console.error();
+        }
     }
     const handleClick=(e)=>{
         const {name,value}=e.target;
@@ -40,10 +45,10 @@ function SignIn() {
 
             <h2>I already have an account</h2>
             <span>Sign in with your email and password</span>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSignINEmailandPass}>
                 <FormComp name="Email" type="email" value={UserDetails.Email} required handleChange={handleClick} label="Email"/>
                 <FormComp name="Password" type="password" value={UserDetails.Password} required handleChange={handleClick} label="Password"/>
-                <Button>SIGN-IN</Button>
+                <Button type="submit">SIGN-IN</Button>
                 <Button style={{backgroundColor:"rgb(66,133,244)"}} onClick={handlesign}>SIGN-IN WITH GOOGLE</Button>
             </form>
             
